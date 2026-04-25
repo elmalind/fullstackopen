53a82ecba096c391c3e4e6c0aa3f7bfb9194e693
elmalind <emma.lindqvist@elisanet.fi>
2026-04-25 12:43:10 +0300

    ex 3.9

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,5 +1,10 @@
-const http = require("http");
-app.requestTimeout(morgan("tiny"));
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("tiny"));
 
 const persons = [
   {
@@ -7,19 +12,16 @@ const persons = [
     number: "040-123456",
     id: "1",
   },
-
   {
     name: "Ada Lovelace",
     number: "39-44-5323523",
     id: "2",
   },
-
   {
     name: "Dan Abramov",
     number: "12-43-234345",
     id: "3",
   },
-
   {
     name: "Marja Poppendieck",
     number: "39-23-6423122",
@@ -27,79 +29,55 @@ const persons = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  if (request.url === "/api/persons" && request.method === "GET") {
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(JSON.stringify(persons));
-  } else if (request.url === "/info") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end(`
-      <p>Phonebook has info for ${persons.length} people</p>
-      <p>${new Date()}</p>
-    `);
-  } else if (
-    request.url.startsWith("/api/persons/") &&
-    request.method === "DELETE"
-  ) {
-    const id = request.url.split("/")[3];
-    const person = persons.find((p) => p.id === id);
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
 
-    if (person) {
-      persons.splice(persons.indexOf(person), 1);
-      response.writeHead(204); // 204 = No Content (poistettu onnistuneesti)
-      response.end();
-    } else {
-      response.writeHead(404);
-      response.end(JSON.stringify({ error: "not found" }));
-    }
-  } else if (
-    request.url.startsWith("/api/persons/") &&
-    request.method === "GET"
-  ) {
-    const id = request.url.split("/")[3];
-    const person = persons.find((p) => p.id === id);
+app.get("/info", (req, res) => {
+  res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `);
+});
 
-    if (person) {
-      response.writeHead(200, { "Content-Type": "application/json" });
-      response.end(JSON.stringify(person));
-    } else {
-      response.writeHead(404);
-      response.end(JSON.stringify({ error: "not found" }));
-    }
-  } else if (request.url === "/api/persons" && request.method === "POST") {
-    let body = "";
-
-    request.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    request.on("end", () => {
-      const newPerson = JSON.parse(body);
-
-      if (!newPerson.name || !newPerson.number) {
-        response.writeHead(400, { "Content-Type": "application/json" });
-        return response.end(
-          JSON.stringify({ error: "name or number missing" }),
-        );
-      }
-
-      if (persons.find((p) => p.name === newPerson.name)) {
-        response.writeHead(409, { "Content-Type": "application/json" });
-        return response.end(JSON.stringify({ error: "name must be unique" }));
-      }
-
-      if (persons.find((p) => p.number === newPerson.number)) {
-        response.writeHead(409, { "Content-Type": "application/json" });
-        return response.end(JSON.stringify({ error: "number must be unique" }));
-      }
-
-      newPerson.id = String(Math.floor(Math.random() * 1000000));
-      persons.push(newPerson);
-
-      response.writeHead(201, { "Content-Type": "application/json" });
-      response.end(JSON.stringify(newPerson));
-    });
+app.get("/api/persons/:id", (req, res) => {
+  const person = persons.find((p) => p.id === req.params.id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).json({ error: "not found" });
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const person = persons.find((p) => p.id === req.params.id);
+  if (person) {
+    persons.splice(persons.indexOf(person), 1);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+app.post("/api/persons", (req, res) => {
+  const newPerson = req.body;
+
+  if (!newPerson.name || !newPerson.number) {
+    return res.status(400).json({ error: "name or number missing" });
+  }
+
+  if (persons.find((p) => p.name === newPerson.name)) {
+    return res.status(409).json({ error: "name must be unique" });
+  }
+
+  if (persons.find((p) => p.number === newPerson.number)) {
+    return res.status(409).json({ error: "number must be unique" });
+  }
+
+  newPerson.id = String(Math.floor(Math.random() * 1000000));
+  persons.push(newPerson);
+
+  res.status(201).json(newPerson);
 });
 
 const PORT = 3001;
