7ff39f95e1dace84fe0fd37565bd41431f417092
elmalind <emma.lindqvist@elisanet.fi>
2026-04-25 15:29:16 +0300

    ex 3.12 testaus

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const Person = require("./models/person");
 
 const app = express();
 
@@ -7,78 +8,64 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("dist"));
 
-const persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: "1",
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: "2",
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: "3",
-  },
-  {
-    name: "Marja Poppendieck",
-    number: "39-23-6423122",
-    id: "4",
-  },
-];
-
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/info", (req, res) => {
-  res.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-  `);
+  Person.find({}).then((persons) => {
+    res.send(`
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+    `);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const person = persons.find((p) => p.id === req.params.id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).json({ error: "not found" });
-  }
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).json({ error: "not found" });
+      }
+    })
+    .catch(() => res.status(400).json({ error: "malformatted id" }));
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const person = persons.find((p) => p.id === req.params.id);
-  if (person) {
-    persons.splice(persons.indexOf(person), 1);
-    res.status(204).end();
-  } else {
-    res.status(404).json({ error: "not found" });
-  }
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(() => res.status(400).json({ error: "malformatted id" }));
 });
 
 app.post("/api/persons", (req, res) => {
-  const newPerson = req.body;
+  const { name, number } = req.body;
 
-  if (!newPerson.name || !newPerson.number) {
+  if (!name || !number) {
     return res.status(400).json({ error: "name or number missing" });
   }
 
-  if (persons.find((p) => p.name === newPerson.name)) {
-    return res.status(409).json({ error: "name must be unique" });
-  }
+  Person.findOne({ name }).then((existing) => {
+    if (existing) {
+      return res.status(409).json({ error: "name must be unique" });
+    }
 
-  if (persons.find((p) => p.number === newPerson.number)) {
-    return res.status(409).json({ error: "number must be unique" });
-  }
+    Person.findOne({ number }).then((existingNum) => {
+      if (existingNum) {
+        return res.status(409).json({ error: "number must be unique" });
+      }
 
-  newPerson.id = String(Math.floor(Math.random() * 1000000));
-  persons.push(newPerson);
-
-  res.status(201).json(newPerson);
+      const person = new Person({ name, number });
+      person.save().then((saved) => {
+        res.status(201).json(saved);
+      });
+    });
+  });
 });
 
 const PORT = process.env.PORT || 3001;
