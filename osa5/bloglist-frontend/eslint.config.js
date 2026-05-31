e3a42ccab15cf91afea866a45f2484361ee5f94c
elmalind <emma.lindqvist@elisanet.fi>
2026-05-31 12:33:01 +0300

    teht 5.12 ESLint

--- osa5/bloglist-frontend/eslint.config.js
+++ osa5/bloglist-frontend/eslint.config.js
@@ -1,10 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -17,13 +18,28 @@ export default [
       }
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh
     },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/prop-types': 'off',
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^[A-Z_]'
+        }
+      ],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true }
